f446ec3fa35e0dcf85bc027c0d5a0b21abc340ec
Nathan <nathandenholm@live.co.uk>
2019-07-29 21:11:10 +0100

    enemies can move ish

--- SpaceInvadersGame.py
+++ SpaceInvadersGame.py
@@ -9,6 +9,8 @@ WINDOW = pygame.display.set_mode((800,600))
 clock = pygame.time.Clock()
 BLACK = 0, 0, 0
 GREEN = 78, 255, 87
+left = False
+right = True
 
 class Ship:
     def __init__(self,x, y):
@@ -36,8 +38,9 @@ class Enemy:
         self.w = 50
         self.h =50
         self.enemy_type = enemy_type
-        self.img = enemys[self.enemy_type][0] # ----> lol 
-
+        self.img = enemys[self.enemy_type][0] # ----> lol
+        self.velocity = 15
+        
     def draw(self):
         game.win.blit(self.img,(self.x, self.y))
         
@@ -61,7 +64,10 @@ class SpaceInvaders:
         self.block3 = Block(425,410,100,50)
         self.block4 = Block(600,410,100,50)
         self.enemys =[]
-    
+        self.move_event = pygame.USEREVENT + 1
+        self.left = False 
+        self.right = True
+        self.velocity = 10
 
     def create_enemys(self):
         x = 153
@@ -69,32 +75,22 @@ class SpaceInvaders:
         for _ in range(11):
             self.enemys.append(Enemy(x, 100, "enemy1"))
             x += 45
-        x = 150
+        x = 153
         for enemy in range(11):
             self.enemys.append(Enemy(x, 135, "enemy2"))
             x += 45
-        x = 150 
+        x = 153 
         for enemy in range(11):
             self.enemys.append(Enemy(x, 175, "enemy2"))
             x+=45
-        x = 135
+        x = 153
         for enemy in range(11):
             self.enemys.append(Enemy(x, 220, "enemy3"))
-            x += 48
-        x = 135
+            x += 45
+        x = 153
         for enemy in range(11):
             self.enemys.append(Enemy(x, 265, "enemy3"))
-            x += 48
-
-
-       
-      
-
-                
-
-            
-            
-
+            x += 45
 
     def redraw(self):
         pygame.Surface.fill(self.win,(BLACK))
@@ -107,15 +103,53 @@ class SpaceInvaders:
         for enemy in self.enemys:
             enemy.draw()
 
+    def move_enemys(self):
+      
+        for enemy in self.enemys:
+            if enemy.x > 700:
+                #self.right = False
+                #self.left = False
+                self.velocity = -15
+
+        
+            elif enemy.x < 100:
+                #self.right == True
+                #self.left == False
+                self.velocity = 15
+
+        for enemy in self.enemys:
+            enemy.x += self.velocity
+    
+
+    
+
+           
+            
+
+
+    
+            
+           
+                
+
 
 
     def main_loop(self):
         self.create_enemys()
+        pygame.time.set_timer(self.move_event, 1000)
         while True:
+            
             clock.tick(60)
             self.ship.move()
-            
             self.redraw()
+            
+            
+            
+            
+
+            
+           
+        
             
 
 
@@ -123,6 +157,10 @@ class SpaceInvaders:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     quit()
+                if event.type == self.move_event:
+                    self.move_enemys()
+                    print("move ships sideways")
+               
             pygame.display.update()
 
 game = SpaceInvaders()
