4be3479f0d25945609bbc24b9064d4076ae19eef
Nathan <nathandenholm@live.co.uk>
2019-08-07 22:02:38 +0100

    images updated

--- images.py
+++ images.py
@@ -1,20 +1,21 @@
 import pygame
 
 ship = pygame.image.load("ship.png")
+small_ship = pygame.transform.scale(ship,(25, 30))
 enemys = {"enemy1":[pygame.transform.scale(pygame.image.load ("enemy1_1.png"),(30,30)),
-                    pygame.image.load ("enemy1_2.png"),
-                    pygame.image.load ("explosionpurple.png"),("hello")
-                    ],
+                    pygame.transform.scale(pygame.image.load ("enemy1_2.png"),(30,30)),
+            ],
         
         "enemy2": [  pygame.transform.scale(pygame.image.load ("enemy2_1.png"),(40,40)),
-                     pygame.image.load ("enemy2_2.png"),
-                     pygame.image.load ("explosionblue.png")],
+                      pygame.transform.scale(pygame.image.load ("enemy2_2.png"),(40,40)),
+                     ],
 
         "enemy3":    [pygame.transform.scale(pygame.image.load ("enemy3_1.png"),(44,44)),
-                     pygame.image.load ("enemy3_2.png"),
-                     pygame.image.load ("explosiongreen.png")]}
+                      pygame.transform.scale(pygame.image.load ("enemy3_2.png"),(44,44)),]
+                     }
 
 bonus = pygame.image.load("mystery.png")
 
+laser = pygame.image.load("laser.png")
 
-print(enemys["enemy1"][0])
+
